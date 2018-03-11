623eea9a75ca590363ead4317aefdcafa10e536c
Francesco Bianco <info.francescobianco@gmail.com>
2018-03-11 09:43:20 +0100

    asd

--- install.sh
+++ install.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 apt update
+apt install -y curl git
 
-useradd -m -p <encryptedPassword> -s /bin/bash <user>
-
-curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64
+adduser --disabled-password --gecos "" vegalive
 
 mkdir -p /opt/dropbox
+mkdir -p /home/vegalive/Dropbox
 
-tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox
-
+cd /opt
+git clone https://github.com/circolodelre/vegalive/
+cd vegalive
 cp dropbox /etc/init.d/
-
 chmod +x /etc/init.d/dropbox
-
-echo DROPBOX_USERS=\"root\" > /etc/default/dropbox
-
+echo DROPBOX_USERS=\"vegalive\" > /etc/default/dropbox
 update-rc.d dropbox defaults
+
+curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64
+tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox
 
 /opt/dropbox/dropboxd
 
