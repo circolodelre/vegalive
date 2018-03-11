dee9ca1e2a6a9b982dc07f602e3e46f61c18c389
Francesco Bianco <info.francescobianco@gmail.com>
2018-03-11 09:33:23 +0100

    deploy

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-git pull
+apt update
 
-#curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64
+useradd -m -p <encryptedPassword> -s /bin/bash <user>
 
-#mkdir -p /opt/dropbox
+curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64
 
-#tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox
+mkdir -p /opt/dropbox
 
-#/opt/dropbox/dropboxd
+tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox
 
 cp dropbox /etc/init.d/
 
 chmod +x /etc/init.d/dropbox
 
 echo DROPBOX_USERS=\"root\" > /etc/default/dropbox
+
+update-rc.d dropbox defaults
+
+/opt/dropbox/dropboxd
+
+service dropbox start
