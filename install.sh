7542eb47b37a8381f6d1ad5ec13514205ec24998
Francesco Bianco <info.francescobianco@gmail.com>
2018-03-11 07:46:57 +0100

    deploy

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 git pull
 
-curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64
+#curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64
 
-mkdir -p /opt/dropbox
+#mkdir -p /opt/dropbox
 
-tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox
+#tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox
 
-/opt/dropbox/dropboxd
+#/opt/dropbox/dropboxd
 
+cp dropbox /etc/init.d/
